9b87e9940e031bd0b7537a9565e711795ac8efb9
Colin Smith <lorddragonsbane@gmail.com>
2015-10-25 18:33:26 -0400

    files added

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfolioWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the portfolioWebsiteApp
+ */
+angular.module('portfolioWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
