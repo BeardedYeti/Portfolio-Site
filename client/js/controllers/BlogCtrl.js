b739d6b6678a19fc7d4c262dbedad050e3ece4a1
Colin Smith <lorddragonsbane@gmail.com>
2015-12-10 19:31:37 -0500

    Blog posts now use ng-click and ng-show to open blog posts

--- client/js/controllers/BlogCtrl.js
+++ client/js/controllers/BlogCtrl.js
@@ -1,6 +1,16 @@
 angular.module('BlogCtrl', []).controller('BlogCtrl', ['$scope', '$http', 'BlogService', function($scope, $http, BlogService) {
-  $scope.title = "Blog"
   var blog = this;
+
+  //Get all Blog Posts
+  blog.posts = {};
+  BlogService.get()
+  .success(function(data) {
+    blog.posts = data;
+  })
+  .error(function(data) {
+    console.log('Error: ' + data);
+  });
+
   blog.tab = 'blog';
 
   blog.selectTab = function(setTab){
@@ -12,23 +22,14 @@ angular.module('BlogCtrl', []).controller('BlogCtrl', ['$scope', '$http', 'BlogS
     return blog.tab === checkTab;
   };
 
-  $scope.formData = {};
-
-//Get all Blog Posts
-  BlogService.get()
-  .success(function(data) {
-    $scope.blog = data;
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-  });
+  blog.formData = {};
 
 //When Submitting the add form, send the text to the node API
-  $scope.createBlog = function() {
+  blog.createBlog = function() {
     BlogService.create()
     .success(function(data) {
-      $scope.formData = {};
-      $scope.blog = data;
+      blog.formData = {};
+      blog.posts = data;
       console.log(data);
     })
     .error(function(data) {
@@ -37,15 +38,15 @@ angular.module('BlogCtrl', []).controller('BlogCtrl', ['$scope', '$http', 'BlogS
   };
 
 //Delete a Blog Post
-$scope.deleteBlog = function(id) {
-  BlogService.delete()
-  .success(function(data) {
-    $scope.blog = data;
-  })
-  .error(function(data) {
-    console.log('Error: ' + data);
-  });
-};
+  blog.deleteBlog = function(id) {
+    BlogService.delete()
+    .success(function(data) {
+      blog.posts = data;
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+  };
 
 
 }]);
