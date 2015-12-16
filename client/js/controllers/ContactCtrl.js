9c5113eec96ded8477e9b7215c532fa146bf1286
Colin Smith <lorddragonsbane@gmail.com>
2015-12-15 20:39:09 -0500

    Fixed some images

--- client/js/controllers/ContactCtrl.js
+++ client/js/controllers/ContactCtrl.js
@@ -1,5 +1,5 @@
 angular.module('ContactCtrl', []).controller('ContactCtrl', ['$scope', function($scope) {
-  $scope.bostonimage = "img/contact/boston.jpg";
+  $scope.bostonimage = "img/contact/boston.gif";
   $scope.colinemail = "cmsgoboston41@gmail.com";
   $scope.colinphone = "(603) 801-1325"
 }]);
